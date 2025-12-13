043e07bb304cf0f74c86c700027ba0959f51ce67
ninhgiang225 <ngnguy26@colby.edu>
2025-12-13 17:09:02 -0500

    Fix DATABASE_URL for Heroku

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config: 
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'mulevents.db')
+    uri = os.environ.get("DATABASE_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri or "sqlite:///mulevents.db"
     UPLOAD_FOLDER = os.path.join(basedir, 'static', 'images')
     QRCODE_FOLDER = os.path.join(basedir, 'static', 'qrcodes')
-    
